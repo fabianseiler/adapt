73f693e12dd488358be3b52449048ee3fceb1370
fabianseiler <Shyperhd@gmail.com>
2025-10-24 09:31:18 +0200

    approchs check

--- examples/eval_sweep.py
+++ examples/eval_sweep.py
@@ -78,7 +78,7 @@ data_t = DataLoader(trainset_1, batch_size=128, shuffle=False, num_workers=0)
 acc_sweep = []
 acc_train_epoch = []
 
-for axx_mult in ['SIAFA1_5', 'SIAFA2_5', 'SIAFA3_5', 'SIAFA4_5', 'SAFAN_5', 'SSAX1_5', 'SSAX2_5', 'SSAX3_5', 'SAID1_5', 'SAID2_5', 'NC_5', 'NCp_5', 'ApprOchs']:
+for axx_mult in ['ApprOchs_5']: #['SIAFA1_5', 'SIAFA2_5', 'SIAFA3_5', 'SIAFA4_5', 'SAFAN_5', 'SSAX1_5', 'SSAX2_5', 'SSAX3_5', 'SAID1_5', 'SAID2_5', 'NC_5', 'NCp_5', 'ApprOchs_5']:
 
 
     #Step 4: Loading the ResNet18 Network for evaluation
